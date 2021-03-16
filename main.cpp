ac585e76dc83b51ac86bf59a5d6f504903f3bc02
yikezawa <y.ikezawa93@gmail.com>
2021-03-16 22:25:56 +0900

    mod

--- main.cpp
+++ main.cpp
@@ -496,3 +496,38 @@ void interpolation_search(){
         std::cout << "Number " << x << " not found";
 }
 
+
+#include "queue"
+#include "vector"
+#include "cstdio"
+
+#define INF 10000010
+vector<pair<int, int>> graph[5 * 100001];
+int dis[5*100001];
+
+int dij(vector<pair<int, int>> *v, int s, int *dis) {
+    priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> pq;
+    pq.push(make_pair(0, s));
+    dis[s] = 0;
+    int u;
+    while(!pq.empty()) {
+        u = (pq.top()).second;
+        pq.pop();
+        for (auto it = v[u].begin(); it != v[u].end(); it++) {
+            if (dis[u] + it-> first < dis[it-> second]) {
+                dis[it->second] = dis[u] + it->first;
+                pq.push(make_pair(dis[it->second], it->second));
+            }
+        }
+    }
+}
+
+void djikstra() {
+    int m, n, l, x, y, s;
+    cin >> n >> m;
+    for (int i = 0; i < m; i++) {
+        scanf("%d%d%d", &x, &y, &l);
+        graph[x].push_back(make_pair(l, y));
+
+    }
+}
