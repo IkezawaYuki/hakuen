ab994d90cdbb0492a0ea1f3029590c9a17743215
yikezawa <y.ikezawa93@gmail.com>
2021-03-21 15:47:14 +0900

    mod

--- main.cpp
+++ main.cpp
@@ -889,6 +889,35 @@ Car& Car::operator=(const Car &c) {
 }
 
 
+template <class T>
+class Array{
+private:
+    T data[5];
+public:
+    void setData(int num, T d);
+    T getData(int num);
+};
+
+template <class T>
+void Array<T>::setData(int num, T d) {
+    if (num < 0 || num > 4) {
+        cout << "配列の範囲を超えています。\n";
+    } else {
+        data[num] = d;
+    }
+}
+
+template <class T>
+T Array<T>::getData(int num) {
+    if (num < 0 || num > 4) {
+        cout << "配列の範囲を超えています。\n";
+        return data[0];
+    }else {
+        return data[num];
+    }
+}
+
+
 
 int main(){
     Point p1(1, 2);
@@ -906,6 +935,8 @@ int main(){
 
     Car car2("car2", 0,0);
     car2 = mycar;
+
+
 
     return 0;
 }
