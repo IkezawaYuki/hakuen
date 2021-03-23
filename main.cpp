f8d41991133b8a1ce004d61e95a2e174922c1535
yikezawa <y.ikezawa93@gmail.com>
2021-03-23 23:21:31 +0900

    mod

--- main.cpp
+++ main.cpp
@@ -1,7 +1,32 @@
-#include <SFML/Graphics.hpp>
-#include <time.h>
-using namespace sf;
+#include <iostream>
+#include <vector>
+#include <cmath>
+
+using namespace std;
+using Graph = vector<vector<int>>;
+
+int N;
+vector<long long> w;
+Graph G;
+
+vector<int> dp1, dp2;
+
+void dfs(int v, int p = -1) {
+    for (auto ch : G[v]) {
+        if (ch == p) continue;
+        dfs(ch, v);
+    }
+
+    dp1[v] = 0, dp2[v] = w[v];
+    for (auto ch : G[v]) {
+        if (ch == p) continue;
+        dp1[v] += max(dp1[ch], dp2[ch]);
+        dp2[v] += dp1[ch];
+    }
+}
 
 int main() {
+    cin >> N;
+
 
 };
