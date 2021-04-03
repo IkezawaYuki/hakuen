d61e3cb414831e6f434c7dab26814842e6b9e4c3
yikezawa <y.ikezawa93@gmail.com>
2021-04-03 23:39:20 +0900

    mod

--- main.cpp
+++ main.cpp
@@ -47,12 +47,25 @@ int heavy_class::get() const {
     return m_cache;
 }
 
+class A {
+    int v;
+public:
+    void set(int value);
+    int get() const;
+};
+
+void A::set(int value) {
+    v = value;
+}
+
+int A::get() const{
+    return v;
+}
+
 int main(){
-    heavy_class heavy_object;
-    heavy_object.set(100);
-    cout << heavy_object.get() << endl;
-    cout << heavy_object.get() << endl;
-    heavy_object.set(200);
-    cout << heavy_object.get() << endl;
-    cout << heavy_object.get() << endl;
+    A a;
+    a.set(42);
+
+    const A& ca = a;
+    ca.get();
 }
