f4b6e548b816c75b7bc2a716a1c4b41785d8aa18
yikezawa <y.ikezawa93@gmail.com>
2021-04-04 20:00:40 +0900

    mod

--- main.cpp
+++ main.cpp
@@ -6,9 +6,12 @@ using namespace std;
 class person {
     std::string m_name;
     int m_age;
-
+    person(int age);
 public:
     person();
+    person(std::string name, int age);
+    ~person();
+
     void set_name(std::string name);
     void set_age(int age);
 
@@ -16,8 +19,22 @@ public:
     int age() const;
 };
 
-person::person():m_age(-1) {
-    cout << "コンストラクター呼び出し" << endl;
+person::person(int age) : m_age(age) {
+    cout << "共通のコンストラクター呼びだし" << endl;
+}
+
+person::person() :person(-1) {
+    cout << "引数なしコンストラクター呼び出し" << endl;
+}
+
+person::person(std::string name, int age) : person(age) {
+    cout << "引数付きコンストラクタの呼び出し" << endl;
+    set_name(name);
+}
+
+
+person::~person() {
+    cout << "デストラクター呼び出し" << endl;
 }
 
 void person::set_name(std::string name) {
@@ -32,15 +49,11 @@ std::string person::name() const {
     return m_name;
 }
 
-int person::age() const {
+int person::age() const{
     return m_age;
 }
 
 int main(){
-    person bob;
-    cout << "初期化直後の年齢: " << bob.age() << endl;
-    bob.set_name("bob");
-    bob.set_age(20);
-    cout << "名前:" << bob.name() << endl;
-    cout << "年齢:" << bob.age() << endl;
+    person alice("alice", 15);
+    cout << alice.name() << endl;
 }
