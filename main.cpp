e7f549ac5015278e2807e2bf6395428d12cabae5
yikezawa <y.ikezawa93@gmail.com>
2021-04-05 22:55:02 +0900

    mod

--- main.cpp
+++ main.cpp
@@ -11,6 +11,7 @@ public:
     person();
     person(std::string name, int age);
     ~person();
+    person(const person& other);
 
     void set_name(std::string name);
     void set_age(int age);
@@ -32,6 +33,11 @@ person::person(std::string name, int age) : person(age) {
     set_name(name);
 }
 
+person::person(const person& other) {
+    cout << "コピーコンストラクター呼び出し" << endl;
+    set_name(other.name());
+    set_age(other.age());
+}
 
 person::~person() {
     cout << "デストラクター呼び出し" << endl;
@@ -53,7 +59,53 @@ int person::age() const{
     return m_age;
 }
 
+class A {
+    int m_v;
+public:
+    A(int);
+    int v() const;
+};
+
+A::A(int v) : m_v(v) {
+
+}
+
+int A::v() const {
+    return m_v;
+}
+
 int main(){
     person alice("alice", 15);
+    person copy(alice);
     cout << alice.name() << endl;
+    cout << copy.name() << endl;
+
+    A x = 42;
+    if (x.v() == 42) {
+        cout << "A.v()は42です" << endl;
+    } else {
+        cout << "A.v()は42ではありません" << endl;
+    }
+}
+
+class Book {
+    string title;
+    string writer;
+    int price;
+
+public:
+    Book(string title, string writer, int price);
+    Book(const Book& other);
+};
+
+Book::Book(string t, string w, int p) {
+    title = t;
+    writer = w;
+    price = p;
+}
+
+Book::Book(const Book& other) {
+    title = other.title;
+    writer = other.writer;
+    price = other.price;
 }
