62d6d6657d03eaeed9174ade7578bc68448bd260
yikezawa <y.ikezawa93@gmail.com>
2021-04-07 23:21:54 +0900

    mod

--- main.cpp
+++ main.cpp
@@ -1,60 +1,38 @@
 #include <iostream>
+using namespace std;
 
-class Base{
-    int value = 0;
-
+class A{
+    union {
+        int i;
+        float f;
+    };
+    long l;
 public:
-    void set_value(int value);
-    int get_value()const;
-};
-
-void Base::set_value(int value) {
-    this->value = value;
-}
-
-int Base::get_value() const {
-    return value;
-}
-
-struct Derived: Base{
-    Derived();
-    ~Derived();
-};
-
-Derived::Derived() {
-    std::cout << "コンストラクター" << std::endl;
-}
-
-Derived::~Derived() {
-    std::cout << "デストラクター" << std::endl;
-}
-
-union U {
-    float f;
-private:
-    int i;
-public:
-    U();
+    A();
     int get_i() const;
+    void set_f(float f);
+    long get_l() const;
 };
 
-U::U() : i(0xdeadbeef) {
-    std::cout << "&f: " << &f << std::endl
-    << "&i: " << &i << std::endl;
+A::A() : i(0xdeadbeef), l(0xc0ffee) {
+    cout << "&f: " << &f << endl
+         << "&i: " << &i << endl
+         << "&l: " << &l << endl;
 }
 
-int U::get_i() const {
-    return this->i;
+int A::get_i() const {
+    return i;
+}
+
+void A::set_f(float f) {
+    this->f = f;
+}
+
+long A::get_l() const {
+    return l;
 }
 
 int main(){
-    Derived d;
-    std::cout << d.get_value() << std::endl;
-    d.set_value(42);
-    std::cout << d.get_value() << std::endl;
-
-    U u;
-    std::cout << std::hex << u.get_i() << std::endl;
-    u.f = 2.71828f;
-    std::cout << std::hex << u.get_i() << std::endl;
+    A a;
+    cout << "i: " << hex << a.get_i() << endl;
 }
