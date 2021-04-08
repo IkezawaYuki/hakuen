8b5351ec65d7a134a07f8ec05b8f12d12162465e
yikezawa <y.ikezawa93@gmail.com>
2021-04-08 21:28:27 +0900

    mod

--- main.cpp
+++ main.cpp
@@ -2,34 +2,76 @@
 
 using namespace std;
 
-class S{
-    static int count;
+string get_message();
+
+class A{
 public:
-    S();
-    ~S();
-    static void show_count();
+    ~A();
 };
 
-int S::count = 0;
-
-S::S(){
-    ++count;
+A::~A() {
+    cout << "デストラクター呼び出し" << endl;
 }
 
-S::~S() {
-    --count;
+void function_scope(int i) {
+    A a;
+    cout << "関数スコープ" << endl;
+    if (i > 0) {
+        return;
+    }
+    cout << "if文のあと" << endl;
 }
 
-void S::show_count() {
-    cout << "S::count: " << count << endl;
+
+int main(){
+    cout << "__FILE__:" << __FILE__ << endl;
+    cout << "__LINE__:" << __LINE__ << endl;
+    cout << "__func__:" << __func__ << endl;
+    cout << "__cplusplus:" << __cplusplus << endl;
+    int line = __LINE__;
+    cout << "line:" << line << ", __LINE__" << __LINE__ << endl;
+
+    auto msg = get_message();
+    cout << msg << endl;
+
+    function_scope(10);
+    cout << endl;
+    function_scope(-10);
+
+    if (true) {
+        cout << "if文" << endl;
+        A a;
+    }
+
+    cout << endl;
+
+    for (int i = 0; i < 5;i++) {
+        cout << "for文(" << i << ")" << endl;
+        A a;
+    }
+
+    cout << endl;
+
+    {
+        cout << "ただのスコープ" << endl;
+        A a;
+    }
+
+    cout << endl;
+
+    cout << "main()関数の最後" << endl;
+
+    int i = 42;
+    {
+        int i = 72;
+
+        cout << "innner: " << &i << ": " << i << endl;
+        i = 0;
+    }
+
+    cout << "outer: " << &i << ": " << i << endl;
 }
 
-int main() {
-    S::show_count();
-    S a;
-    a.show_count();
-    S b;
-    b.show_count();
-    S c;
-    c.show_count();
+string get_message(){
+    return "hello, forward declaration";
 }
