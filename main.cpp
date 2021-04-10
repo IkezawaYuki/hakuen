65aeef1594e3164c173e36b4a90229b11372d8b5
yikezawa <y.ikezawa93@gmail.com>
2021-04-10 12:37:54 +0900

    mod

--- main.cpp
+++ main.cpp
@@ -1,68 +1,21 @@
 #include <iostream>
-#include <vector>
 
-class A{
-    std::string m_name;
-    int m_value;
-
-public:
-    explicit A(std::string name, int value);
-    explicit A(std::string name);
-    A();
-    void show() const;
-};
-
-A::A(std::string name, int value) :m_name(name), m_value(value)
+int* local()
 {
+    int i = 42;
+    return &i;
 }
 
-A::A(std::string name) :A(name, -1)
+int* dyn_alloc()
 {
+    int* ptr = new int;
+    *ptr = 42;
+    return ptr;
 }
 
-A::A(): A("default")
+int main()
 {
-}
-
-void A::show() const {
-    std::cout << m_name << " " << m_value << std::endl;
-}
-
-int main(){
-    A a[4] =
-            {
-            A{"first", 42},
-            A{"second"},
-
-            };
-
-    a[0].show();
-    a[1].show();
-    a[2].show();
-    a[3].show();
-
-    std::vector<int> empty;
-    std::cout << "empty.size(): " << empty.size() << std::endl;
-
-    std::vector<int> array = {10, 20, 30, 40, 50};
-    std::cout << "array.size(): " << array.size() << std::endl;
-
-
-    for (int i : array) {
-        std::cout << i << std::endl;
-    }
-
-    array.push_back(42);
-    array.push_back(9);
-    for (int i : array) {
-        std::cout << i << std::endl;
-    }
-
-    std::cout  << std::endl;
-
-    array.pop_back();
-    for (int i : array) {
-        std::cout << i << std::endl;
-    }
-
+    int* d = dyn_alloc();
+    std::cout << *d << std::endl;
+    delete d;
 }
