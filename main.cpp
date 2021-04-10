be9de9bd96330384bb51e0af012aa8a716853c9f
yikezawa <y.ikezawa93@gmail.com>
2021-04-10 20:36:04 +0900

    mod

--- main.cpp
+++ main.cpp
@@ -1,217 +1,195 @@
+#include <memory>
+#include <utility>
 #include <iostream>
 
-class Integer
-{
-    int value;
+class A{
+public:
+    A()
+    {
+        std::cout << "コンストラクター" << std::endl;
+    }
 
+    ~A()
+    {
+        std::cout << "デストラクター" << std::endl;
+    }
+};
+
+std::unique_ptr<A> allocate()
+{
+    std::cout << "allocate()" << std::endl;
+    std::unique_ptr<A> ptr{new A{}};
+    return std::move(ptr);
+}
+
+class Counter {
+    int value;
+public:
+    Counter() :value{0} {}
+
+    int operator()();
+    int operator()(int n);
+
+    void show() const;
+};
+
+int Counter::operator()() {
+    return ++value;
+}
+
+int Counter::operator()(int n) {
+    return value += n;
+}
+
+void Counter::show() const {
+    std::cout << "Counter.value" << value << std::endl;
+}
+
+class Integer {
+    int value;
 public:
     Integer(int value) : value{value} {}
+    Integer& operator=(const Integer& rhs);
 
-    Integer operator+(const Integer& other) const;
-    Integer operator-(const Integer& other) const;
-    Integer operator*(const Integer& other) const;
-    Integer operator/(const Integer& other) const;
-
-    Integer operator+() const;
-    Integer operator-() const;
-
-    Integer& operator++();
-    Integer& operator--();
-
-    Integer operator++(int);
-    Integer operator--(int);
-
-    Integer operator|(const Integer& rhs) const;
-    Integer operator&(const Integer& rhs) const;
-    Integer operator^(const Integer& rhs) const;
-
-    Integer operator~() const;
-
-    bool operator==(const Integer& rhs) const;
-    bool operator!=(const Integer& rhs) const;
-
-    bool operator<(const Integer& rhs) const;
-    bool operator>(const Integer& rhs) const;
-    bool operator<=(const Integer& rhs) const;
-    bool operator>=(const Integer& rhs) const;
+    Integer& operator+=(const Integer& rhs);
+    Integer& operator-=(const Integer& rhs);
+    Integer& operator*=(const Integer& rhs);
+    Integer& operator/=(const Integer& rhs);
 
     void show() const;
 };
 
-bool Integer::operator==(const Integer& rhs) const{
-    return value == rhs.value;
-}
-
-bool Integer::operator!=(const Integer &rhs) const {
-    return !(*this == rhs.value);
-}
-
-bool Integer::operator>(const Integer &rhs) const {
-    return rhs < *this;
-}
-
-bool Integer::operator<=(const Integer &rhs) const {
-    return !(rhs < *this);
-}
-
-bool Integer::operator>=(const Integer &rhs) const {
-    return rhs <= *this;
-}
-
-
-bool Integer::operator<(const Integer &rhs) const {}
-
-Integer Integer::operator+(const Integer &other) const {
-    return Integer{value + other.value};
-}
-
-Integer Integer::operator-(const Integer &other) const {
-    return Integer{value - other.value};
-}
-
-Integer Integer::operator*(const Integer &other) const {
-    return Integer{value * other.value};
-}
-
-Integer Integer::operator/(const Integer &other) const {
-    return Integer{value / other.value};
-}
-
-Integer Integer::operator+() const {
+Integer& Integer::operator+=(const Integer &rhs) {
+    value += rhs.value;
     return *this;
 }
 
-Integer Integer::operator-() const {
-    return Integer{-value};
-}
-
-Integer Integer::operator|(const Integer &rhs) const {
-    return Integer{value | rhs.value};
-}
-
-Integer Integer::operator&(const Integer &rhs) const {
-    return Integer{value & rhs.value};
-}
-
-Integer Integer::operator^(const Integer &rhs) const {
-    return Integer{value ^ rhs.value};
-}
-
-Integer Integer::operator~() const {
-    return Integer{~value};
-}
-
-void Integer::show() const
-{
-    std::cout << "Integer.value = " << value << std::endl;
-}
-
-Integer& Integer::operator++() {
-    ++value;
+Integer& Integer::operator-=(const Integer &rhs) {
+    value -= rhs.value;
     return *this;
 }
 
-Integer& Integer::operator--(){
-    --value;
+Integer& Integer::operator*=(const Integer &rhs) {
+    value *= rhs.value;
     return *this;
 }
 
-Integer Integer::operator++(int){
-    auto tmp = *this;
-    ++*this;
-    return tmp;
+Integer& Integer::operator/=(const Integer &rhs) {
+    value /= rhs.value;
+    return *this;
 }
 
-Integer Integer::operator--(int){
-    auto tmp = *this;
-    --*this;
-    return tmp;
+Integer& Integer::operator=(const Integer &rhs) {
+    std::cout << "コピー: " << value << " -> " << rhs.value << std::endl;
+    value = rhs.value;
+    return *this;
 }
 
-class Array
+void Integer::show() const {
+    std::cout << "value: " << value << std::endl;
+}
+
+class heap {
+    int* i;
+
+public:
+    heap() : i{nullptr} { }
+    ~heap()
+    {
+        delete i;
+    }
+    bool create();
+    operator bool() const;
+};
+
+bool heap::create() {
+    if (*this)
+    {
+        return false;
+    }
+    i = new int{};
+    *i = 0;
+    return true;
+}
+
+heap::operator bool() const {
+    return i != nullptr;
+}
+
+class vector4d {
+    float x, y, z, w;
+
+public:
+    explicit vector4d(float x, float y, float z, float w) : x{x}, y{y}, z{z}, w{w} {}
+    friend vector4d add(const vector4d& lhs, const vector4d& rhs);
+};
+
+vector4d add(const vector4d& lhs, const vector4d& rhs)
 {
-    int buffer[100];
-public:
-    int& operator[](int index);
-    std::size_t size() const {return 100;}
-};
-
-int& Array::operator[](int index) {
-    return buffer[index];
+    vector4d ret {
+        lhs.x + rhs.x,
+        lhs.y + rhs.y,
+        lhs.z + rhs.z,
+        lhs.w + rhs.w,
+    };
+    return ret;
 }
 
-class int_observer_ptr
-{
-    int* pointer;
+class vector3d{
+    float x, y, z;
 public:
-    explicit int_observer_ptr(int* pointer) : pointer{pointer} {}
-    int& operator*() const;
+    explicit vector3d(float x, float y, float z) : x{x}, y{y}, z{z}, {}
+    operator vector4d() const;
 };
 
-int& int_observer_ptr::operator*() const {
-    return *pointer;
-}
-
-class A{
-    int value;
-public:
-    explicit A(int value) : value{value} {}
-    void show() const;
-};
-
-void A::show() const {
-    std::cout << "A.value: " << value << std::endl;
-}
-
-class A_observer_ptr
-{
-    A* pointer;
-public:
-    explicit A_observer_ptr(A* pointer) : pointer{pointer} {}
-    A& operator*() const;
-    A* operator->() const;
-};
-
-A& A_observer_ptr::operator*() const {
-    return *pointer;
-}
-
-A* A_observer_ptr::operator->() const {
-    return pointer;
+vector3d::operator vector4d() const {
+    vector4d ret{x, y, z, 0.0f};
+    return ret;
 }
 
 int main(){
+    {
+        std::unique_ptr<A> ptr;
+        std::cout << "関数呼び出しの前" << std::endl;
+        ptr = allocate();
+        std::cout << "関数呼び出しの後" << std::endl;
+    }
+    std::cout << "スコープのあと" << std::endl;
+
+    Counter c;
+    c();
+    c();
+    c();
+    c.show();
+    c(-3);
+    c.show();
+
     Integer x = 10;
     Integer y = 3;
     Integer z = 7;
 
-    auto v = x - -y + +z;
-    v.show();
+    z *= x;
+    y /= y;
+    x -= y;
+    x += z;
 
-    Integer a = 10;
-    ++a;
+    x.show();
 
-    ++a;
-    a.show();
-
-    --a;
-    a.show();
-
-    a++;
-    a.show();
-
-    a--;
-    a.show();
-
-    Array array;
-    for (std::size_t i = 0; i < array.size(); ++i) {
-        array[i] = i;
+    heap h;
+    if (!h) {
+        std::cout << "変換関数がfalseが返しました" << std::endl;
     }
-    std::cout << array[10] << std::endl;
 
-    A k{42};
-    A_observer_ptr pointer{&k};
+    std::cout << "heap::create呼び出し" << std::endl;
+    h.create();
 
-    (*pointer).show();
-    pointer->show();
+    if (!h) {
+        std::cout << "変換関数がfalseが返しました" << std::endl;
+    }
+
+    std::cout << "終了" << std::endl;
+
+    vector3d v1{1,2,3}, v2{0,1,0};
+    vector4d a = v1;
+    vector4d b = v1 + v2;
 }
