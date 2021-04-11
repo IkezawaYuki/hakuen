9ad253aabf5ca2902f564c9ea977fc9c9a25933a
yikezawa <y.ikezawa93@gmail.com>
2021-04-11 17:30:37 +0900

    mod

--- main.cpp
+++ main.cpp
@@ -1,25 +1,68 @@
 #include <iostream>
 
-class Base{
-    virtual void method();
-
+class BaseA{
+    int value;
 public:
-    void call_method(){method();}
+    explicit BaseA(int value) : value{value} {}
+    void show_BaseA()
+    {
+        std::cout << "BaseA.value" << value << std::endl;
+    }
 };
 
-void Base::method() {
-    std::cout << "Base::method()" << std::endl;
-}
-
-class Derived : public Base {
-    void method() override;
+class BaseB{
+    int values;
+public:
+    explicit BaseB(int values): values{values} {}
+    void show_BaseB()
+    {
+        std::cout << "BaseB.value = " << values << std::endl;
+    }
 };
 
-void Derived::method() {
-    std::cout << "Derived::method()" << std::endl;
+class Derived : public BaseA, public BaseB
+{
+public:
+    Derived(int a, int b);
+};
+
+Derived::Derived(int a, int b) :BaseA{a}, BaseB{b} {
+
 }
+
+class Base
+{
+    int value;
+public:
+    Base(int value) : value{value}{}
+    void show();
+};
+
+void Base::show() {
+    std::cout << "this: " << this << std::endl
+    << "this->value: " << value << std::endl;
+}
+
+class DerivedA : public Base{
+public:
+    DerivedA(int value) : Base{value} { }
+};
+
+class DerivedB : public Base{
+public:
+    DerivedB(int value) : Base{value} { }
+};
+
+class MoreDerived: public DerivedA, public DerivedB {
+public:
+    MoreDerived(int d_a, int d_b) : DerivedA{d_a}, DerivedB{d_b}
+    {
+
+    }
+};
 
 int main(){
-    Derived derived;
-    derived.call_method();
+    MoreDerived more_derived{42, 72};
+    more_derived.DerivedA::show();
+    more_derived.DerivedB::show();
 }
