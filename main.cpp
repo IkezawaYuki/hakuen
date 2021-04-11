6525e369b1872fb68b8859745ba1b5bd32553e01
yikezawa <y.ikezawa93@gmail.com>
2021-04-11 23:38:18 +0900

    mod

--- main.cpp
+++ main.cpp
@@ -1,68 +1,33 @@
 #include <iostream>
 
-class BaseA{
-    int value;
-public:
-    explicit BaseA(int value) : value{value} {}
-    void show_BaseA()
-    {
-        std::cout << "BaseA.value" << value << std::endl;
-    }
-};
-
-class BaseB{
-    int values;
-public:
-    explicit BaseB(int values): values{values} {}
-    void show_BaseB()
-    {
-        std::cout << "BaseB.value = " << values << std::endl;
-    }
-};
-
-class Derived : public BaseA, public BaseB
+struct vector2d
 {
-public:
-    Derived(int a, int b);
+    int x;
+    int y;
 };
 
-Derived::Derived(int a, int b) :BaseA{a}, BaseB{b} {
+class Vec2{
+    int m_x;
+    int m_y;
 
+public:
+    int getX() const { return m_x; }
+    int getY() const { return m_y; }
+
+    void setX(int x) { m_x = x; }
+    void setY(int y) { m_y = y; }
+};
+
+template <typename Vector>
+void show_vector(Vector v);
+
+template <>
+void show_vector<Vec2>(Vector v){
+    std::cout << "Vec{x: " << v.getX() << ", y: " << v.getY() << "}"
 }
 
-class Base
+template <typename Vector>
+void show_vector<Vector v)
 {
-    int value;
-public:
-    Base(int value) : value{value}{}
-    void show();
-};
-
-void Base::show() {
-    std::cout << "this: " << this << std::endl
-    << "this->value: " << value << std::endl;
-}
-
-class DerivedA : public Base{
-public:
-    DerivedA(int value) : Base{value} { }
-};
-
-class DerivedB : public Base{
-public:
-    DerivedB(int value) : Base{value} { }
-};
-
-class MoreDerived: public DerivedA, public DerivedB {
-public:
-    MoreDerived(int d_a, int d_b) : DerivedA{d_a}, DerivedB{d_b}
-    {
-
-    }
-};
-
-int main(){
-    MoreDerived more_derived{42, 72};
-    more_derived.DerivedA::show();
-    more_derived.DerivedB::show();
+    std::cout << "{x: " << v.getX() << ", y: " << v.getY() << "}"
 }
